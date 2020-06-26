4331965d9e6b922cce9066e3bb6bb2fcd273ef52
odyssic <stevenbuddenjr@gmail.com>
2020-06-26 13:38:49 -0400

    heroku kinks changing settings

--- config/settings.py
+++ config/settings.py
@@ -130,9 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = ''
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+STATIC_ROOT = ''
 
 STATICFILES_DIRS = (os.path.join('static'), )
 
