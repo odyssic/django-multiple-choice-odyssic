93448471b961fcbcc74cdf82d561294c8da1e520
odyssic <stevenbuddenjr@gmail.com>
2020-06-26 12:50:54 -0400

    secret key change

--- config/settings.py
+++ config/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
+SECRET_KEY = os.getenv('SECRET_KEY', '<old-secret-key>')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
