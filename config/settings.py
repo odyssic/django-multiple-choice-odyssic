e03a1c1e805b22db33f2ac416fb1f94e80124605
odyssic <stevenbuddenjr@gmail.com>
2020-07-07 15:10:22 -0400

    pipenv work

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from pathlib import Path
-
+import django_heroku
 
 import environ
 
@@ -42,7 +42,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'registration',
-    # 'django-heroku',
+    'django-heroku',
     'django.contrib.admin',
     'fontawesome_5',
     'django.contrib.auth',
@@ -191,5 +191,4 @@ DATABASES = {
 
 # Configure Django App for Heroku.
 
-# `django_heroku.settings(locals())`
-# import django_heroku
+`django_heroku.settings(locals())`
